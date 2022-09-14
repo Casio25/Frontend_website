9ee585958aa75d1be43f7e602e89a2a749295e7a
casio25 <mishakolomiets355@gmail.com>
2022-09-14 22:58:23 +0300

    small fixes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-
-function showLikePicture(){
+function showLikePicture() {
     var likeImage = document.getElementById('roflan_like');
     var dislikeImage = document.getElementById('roflan_dislike');
     likeImage.style.display = 'block';
     dislikeImage.style.display = 'none';
 }
 
-function showDislikePicture(){
+function showDislikePicture() {
     var dislikeImage = document.getElementById('roflan_dislike');
     var likeImage = document.getElementById('roflan_like');
+    dislikeImage.style.display = 'block';
+    likeImage.style.display = 'none';
+}
+
+function showLikePicture2() {
+    var likeImage = document.getElementById('roflan_like_2');
+    var dislikeImage = document.getElementById('roflan_dislike_2');
+    likeImage.style.display = 'block';
+    dislikeImage.style.display = 'none';
+}
+
+function showDislikePicture2() {
+    var dislikeImage = document.getElementById('roflan_dislike_2');
+    var likeImage = document.getElementById('roflan_like_2');
     dislikeImage.style.display = 'block';
     likeImage.style.display = 'none';
 }
