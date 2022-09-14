00c2caa48347d7d0d2b4dc0ccb28a7a303d4c480
casio25 <mishakolomiets355@gmail.com>
2022-09-14 20:13:12 +0300

    added like/dislike button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-autoBrands=['BMW', 'Ferrari', "Porshe"];
-for(let brands in autoBrands){
-    console.log(`${brands} => ${autoBrands[brands]}`)
+
+function showLikePicture(){
+    var likeImage = document.getElementById('roflan_like');
+    var dislikeImage = document.getElementById('roflan_dislike');
+    likeImage.style.display = 'block';
+    dislikeImage.style.display = 'none';
+}
+
+function showDislikePicture(){
+    var dislikeImage = document.getElementById('roflan_dislike');
+    var likeImage = document.getElementById('roflan_like');
+    dislikeImage.style.display = 'block';
+    likeImage.style.display = 'none'
 }
