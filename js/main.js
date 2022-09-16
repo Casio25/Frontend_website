4cd891d02f108f4c4d0f63011b07729615bdf3a6
casio25 <mishakolomiets355@gmail.com>
2022-09-16 19:57:04 +0300

    tested likes/dislikes

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
+var likeImage = document.getElementById('roflan_like');
+var dislikeImage = document.getElementById('roflan_dislike');
+var dislikeImage2 = document.getElementById('roflan_dislike_2');
+var likeImage2 = document.getElementById('roflan_like_2');
+
 function showLikePicture() {
-    var likeImage = document.getElementById('roflan_like');
-    var dislikeImage = document.getElementById('roflan_dislike');
     likeImage.style.display = 'block';
     dislikeImage.style.display = 'none';
 }
 
 function showDislikePicture() {
-    var dislikeImage = document.getElementById('roflan_dislike');
-    var likeImage = document.getElementById('roflan_like');
     dislikeImage.style.display = 'block';
     likeImage.style.display = 'none';
 }
 
 function showLikePicture2() {
-    var likeImage = document.getElementById('roflan_like_2');
-    var dislikeImage = document.getElementById('roflan_dislike_2');
-    likeImage.style.display = 'block';
-    dislikeImage.style.display = 'none';
+    likeImage2.style.display = 'block';
+    dislikeImage2.style.display = 'none';
 }
 
 function showDislikePicture2() {
-    var dislikeImage = document.getElementById('roflan_dislike_2');
-    var likeImage = document.getElementById('roflan_like_2');
-    dislikeImage.style.display = 'block';
-    likeImage.style.display = 'none';
+    dislikeImage2.style.display = 'block';
+    likeImage2.style.display = 'none';
 }
