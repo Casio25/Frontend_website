184790500c7b7a798f28265801d59191d325d9c3
casio25 <mishakolomiets355@gmail.com>
2022-09-23 14:57:50 +0300

    window close

--- js/main.js
+++ js/main.js
@@ -35,6 +35,12 @@ span.onclick = function(){
     modal.style.display = 'none';
 }
 
+window.onclick = function(event){
+    if (event.target == modal){
+        modal.style.display ="none"
+    }
+}
+
 
 $(document).ready(function () {
     $("#demo").html("");
